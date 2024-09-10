43e7288380401ab3fae1a816888ca2e30d52aeae
rajesh200311 <rajeshgaming24@gmail.com>
2024-09-10 13:50:14 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -7,10 +7,11 @@ function renderTodoList() {
 
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
-    const { name, dueDate,time } = todoObject;
+    const { name, dueDate,Timer } = todoObject;
     const html = `
       <div class="result-2">${name}</div>
       <div class="result-2">${dueDate}</div>
+      <div class="result-2">${Timer}</div>
       <button onclick="
         todoList.splice(${i}, 1);
         renderTodoList();
@@ -30,8 +31,11 @@ function todo() {
   const dateInputElement = document.querySelector('.date');
   const dueDate = dateInputElement.value;
 
-  if (name.trim() === '' || dueDate.trim() === '' ) {
-    alert('Please fill in all fields (task name, date).');
+  const timeEle = document.querySelector('.times');
+  const Timer = timeEle.value;
+
+  if (name.trim() === '' || dueDate.trim() === ''||Timer.trim()=='' ) {
+    alert('Please fill in all fields (task name, date and time).');
     return;
 
   }
@@ -39,6 +43,7 @@ function todo() {
   todoList.push({
     name,
     dueDate,
+    Timer,
   });
 
   inputElement.value = '';
